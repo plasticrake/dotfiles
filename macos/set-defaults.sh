4988263f8648cc9f5cd4c78c27eea712251cecf6
Patrick Seal <code@plasticrake.com>
2016-10-03 14:19:20 -0700

    adding zshenv

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -43,3 +43,7 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Expanded Save Window
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
